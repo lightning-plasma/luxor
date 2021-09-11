8e7f6e6876da03e02e620423b6218fd32f3769fa
iiohioryuki <hiroyuki.iio@askul.com>
2021-09-11 13:16:53 +0900

    application ymlの値を読み込む

--- src/main/kotlin/com/archetype/luxor/web/controller/LuxorController.kt
+++ src/main/kotlin/com/archetype/luxor/web/controller/LuxorController.kt
@@ -2,6 +2,9 @@ package com.archetype.luxor.web.controller
 
 import com.archetype.luxor.web.entity.Reply
 import org.hibernate.validator.constraints.Length
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.http.MediaType
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
@@ -16,11 +19,20 @@ import javax.validation.constraints.Size
     produces = [MediaType.APPLICATION_JSON_VALUE]
 )
 @Validated
-class LuxorController {
+@EnableConfigurationProperties(value = [CustomLabel::class])
+class LuxorController(
+    private val customLabel: CustomLabel
+) {
     @GetMapping("hello/{name}")
     fun hello(
         @PathVariable("name") @Size(min = 3, max = 10) name: String
     ): Reply {
-        return Reply("Hello, $name")
+        return Reply("Hello ${customLabel.text}, $name")
     }
 }
+
+@ConstructorBinding
+@ConfigurationProperties("reply.label")
+data class CustomLabel(
+    val text: String
+)
