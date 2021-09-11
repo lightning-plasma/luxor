195a9f05acfb0c627755b3d707defc0151e73c50
iiohioryuki <hiroyuki.iio@askul.com>
2021-09-11 11:06:20 +0900

    汎用例外処理を追加

--- src/main/kotlin/com/archetype/luxor/web/advice/ErrorAdvice.kt
+++ src/main/kotlin/com/archetype/luxor/web/advice/ErrorAdvice.kt
@@ -1,18 +1,48 @@
 package com.archetype.luxor.web.advice
 
 import com.archetype.luxor.web.entity.ErrorResponse
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.WebRequest
-import org.springframework.web.servlet.NoHandlerFoundException
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import java.lang.Exception
 
 @RestControllerAdvice
-class ErrorAdvice {
-    @ExceptionHandler(value = [NoHandlerFoundException::class])
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    fun noHandleFound(ex: NoHandlerFoundException, req: WebRequest): ErrorResponse {
-        return ErrorResponse("no handler found")
-    }
+@RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+class CommonErrorAdvice : ResponseEntityExceptionHandler() {
+    override fun handleExceptionInternal(
+        ex: Exception,
+        body: Any?,
+        headers: HttpHeaders,
+        status: HttpStatus,
+        request: WebRequest
+    ): ResponseEntity<Any> =
+        if (body !is ErrorResponse) {
+            val response = when {
+                status.is4xxClientError ->
+                    ErrorResponse("no handler found")
+                status.is5xxServerError ->
+                    ErrorResponse("something wrong ;-(")
+                else ->
+                    ErrorResponse("hmm...")
+            }
+
+            ResponseEntity(response, headers, status)
+        } else {
+            ResponseEntity(body, headers, status)
+        }
+
+    // どこでも拾われなかった例外はここで処理する
+    @ExceptionHandler(value = [Throwable::class])
+    fun handleThrowable(ex: Exception, req: WebRequest): ResponseEntity<Any> =
+        ResponseEntity(
+            ErrorResponse("something wrong ;-("),
+            null,
+            HttpStatus.INTERNAL_SERVER_ERROR,
+        )
 }
