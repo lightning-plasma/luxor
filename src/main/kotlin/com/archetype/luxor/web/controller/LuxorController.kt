168c32caa354f477c2837835180cd9fb5741496a
iiohioryuki <hiroyuki.iio@askul.com>
2021-09-11 10:24:40 +0900

    reply

--- src/main/kotlin/com/archetype/luxor/web/controller/LuxorController.kt
+++ src/main/kotlin/com/archetype/luxor/web/controller/LuxorController.kt
@@ -1,13 +1,17 @@
 package com.archetype.luxor.web.controller
 
 import com.archetype.luxor.web.entity.Reply
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/luxor")
+@RequestMapping(
+    path = ["/luxor"],
+    produces = [MediaType.APPLICATION_JSON_VALUE]
+)
 class LuxorController {
     @GetMapping("hello/{name}")
     fun hello(@PathVariable("name") name: String): Reply {
