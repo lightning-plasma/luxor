abfa7d1377baca4e22f2bfe828f46866975e6c3c
iiohioryuki <hiroyuki.iio@askul.com>
2021-09-25 15:28:54 +0900

    validatorの別実装

--- web/src/main/kotlin/com/archetype/luxor/web/validator/BookValidator.kt
+++ web/src/main/kotlin/com/archetype/luxor/web/validator/BookValidator.kt
@@ -14,12 +14,11 @@ class BookValidator : Validator {
     override fun validate(target: Any, errors: Errors) {
         if (target !is BookRequest) return
 
+        // messages.properties を使う方法がいまいちわからん...
         if (TARGET_PUBLISHER == target.publisher && target.price < LOWEST_PRICE) {
-            errors.rejectValue(
-                "",
-                "request.validation.message",
-                arrayOf(TARGET_PUBLISHER, LOWEST_PRICE, target.price),
-                ""
+            errors.reject(
+                "BookValidator.message",
+                ERROR_MESSAGE.format(TARGET_PUBLISHER, LOWEST_PRICE, target.price)
             )
         }
     }
@@ -27,5 +26,6 @@ class BookValidator : Validator {
     companion object {
         private const val TARGET_PUBLISHER = "みすず書房"
         private const val LOWEST_PRICE = 1000
+        private const val ERROR_MESSAGE = "%sの書籍は%d以上の本を登録できます。Requested price=%d"
     }
 }
