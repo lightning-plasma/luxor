b352ba324aed540d2c4d3c93f875ac4776f6cee8
iiohioryuki <hiroyuki.iio@askul.com>
2021-10-09 19:48:29 +0900

    fix

--- infra/api/src/main/kotlin/com/archetype/luxor/infra/api/entity/BookRatingErrorResponse.kt
+++ infra/api/src/main/kotlin/com/archetype/luxor/infra/api/entity/BookRatingErrorResponse.kt
@@ -3,25 +3,16 @@ package com.archetype.luxor.infra.api.entity
 import com.archetype.luxor.infra.api.client.Failure
 
 data class BookRatingErrorResponse(
-    val resultSet: ResultSet? = null
+    val status: Int,
+    val message: String
 ) : ErrorResponse {
-
-    data class ResultSet(
-        val listResult: List<Result>
-    )
-
-    data class Result(
-        val status: Int,
-        val message: String
-    )
-
-    override fun <R> toFailure(request: Failure.Request, response: Failure.Response):
-        com.archetype.luxor.infra.api.client.Result<R> =
-        this.resultSet?.listResult?.first()?.let {
-            Failure(
-                message = it.message,
-                request = request,
-                response = response
-            )
-        } ?: Failure(request = request, response = response)
+    override fun <R> toFailure(
+        request: Failure.Request,
+        response: Failure.Response
+    ): com.archetype.luxor.infra.api.client.Result<R> =
+        Failure(
+            message = message,
+            request = request,
+            response = response
+        )
 }
