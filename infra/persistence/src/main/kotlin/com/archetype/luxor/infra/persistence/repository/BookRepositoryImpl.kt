731b07e38a62f13e8a1fd9d8a8ddf8eb7779a8ae
lightning-plasma <hiroyuki.iio@askul.com>
2022-11-21 09:59:18 +0900

    suspend functionはadviceが機能しないので消しとく

--- infra/persistence/src/main/kotlin/com/archetype/luxor/infra/persistence/repository/BookRepositoryImpl.kt
+++ infra/persistence/src/main/kotlin/com/archetype/luxor/infra/persistence/repository/BookRepositoryImpl.kt
@@ -5,9 +5,9 @@ import com.archetype.luxor.domain.entity.Book
 import com.archetype.luxor.domain.entity.Isbn
 import com.archetype.luxor.domain.error.NotFoundError
 import com.archetype.luxor.domain.error.ResourceAccessError
-import com.archetype.luxor.infra.persistence.advice.DataAccessExceptionAdvice
 import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.reactive.awaitFirstOrNull
+import org.springframework.dao.DataAccessException
 import org.springframework.dao.DuplicateKeyException
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
 import org.springframework.data.relational.core.query.Criteria.where
@@ -19,8 +19,7 @@ import java.time.LocalDateTime
 class BookRepositoryImpl(
     private val template: R2dbcEntityTemplate,
 ) : BookRepository {
-    @DataAccessExceptionAdvice("BookRepository#fetchAll")
-    override suspend fun fetchAll(): List<Book> =
+    override suspend fun fetchAll(): List<Book> = try {
         template.select(com.archetype.luxor.infra.persistence.entity.Book::class.java)
             .all()
             .collectList()
@@ -36,32 +35,36 @@ class BookRepositoryImpl(
                     rating = null,
                 )
             }
-
-    @DataAccessExceptionAdvice("BookRepository#fetch")
-    override suspend fun fetch(isbn: Isbn): Book {
-        val book = template
-            .select(com.archetype.luxor.infra.persistence.entity.Book::class.java)
-            .from("main.book")
-            .matching(
-                query(
-                    where("isbn").`is`(isbn.asString()),
-                )
-            )
-            .one()
-            .awaitFirstOrNull() ?: throw NotFoundError("${isbn.asString()} is not found")
-
-        return Book(
-            isbn = Isbn(book.isbn),
-            title = book.title,
-            author = book.author,
-            publisher = book.publisher,
-            price = book.price,
-            genre = null,
-            rating = null,
-        )
+    } catch (e: DataAccessException) {
+        throw ResourceAccessError(e, "BookRepository#fetchAllでエラーが発生しました")
     }
 
-    @DataAccessExceptionAdvice("BookRepository#register")
+    override suspend fun fetch(isbn: Isbn): Book =
+        try {
+            val book = template
+                .select(com.archetype.luxor.infra.persistence.entity.Book::class.java)
+                .from("main.book")
+                .matching(
+                    query(
+                        where("isbn").`is`(isbn.asString()),
+                    )
+                )
+                .one()
+                .awaitFirstOrNull() ?: throw NotFoundError("${isbn.asString()} is not found")
+
+            Book(
+                isbn = Isbn(book.isbn),
+                title = book.title,
+                author = book.author,
+                publisher = book.publisher,
+                price = book.price,
+                genre = null,
+                rating = null,
+            )
+        } catch (e: DataAccessException) {
+            throw ResourceAccessError(e, "BookRepository#fetchAllでエラーが発生しました")
+        }
+
     override suspend fun register(book: Book) {
         val now = LocalDateTime.now()
         val entity = com.archetype.luxor.infra.persistence.entity.Book(
@@ -78,22 +81,28 @@ class BookRepositoryImpl(
             template.insert(entity).awaitFirst()
         } catch (e: DuplicateKeyException) {
             throw ResourceAccessError(e, "This book is already registered")
+        } catch (e: DataAccessException) {
+            throw ResourceAccessError(e, "BookRepository#registerでエラーが発生しました")
         }
     }
 
-    @DataAccessExceptionAdvice("BookRepository#insert")
     override suspend fun update(book: Book) {
-        val now = LocalDateTime.now()
-        val entity = com.archetype.luxor.infra.persistence.entity.Book(
-            isbn = book.isbn.asString(),
-            title = book.title,
-            author = book.author,
-            publisher = book.publisher,
-            price = book.price,
-            createdAt = now,
-            updatedAt = now
-        )
+        try {
+            val now = LocalDateTime.now()
+            val entity = com.archetype.luxor.infra.persistence.entity.Book(
+                isbn = book.isbn.asString(),
+                title = book.title,
+                author = book.author,
+                publisher = book.publisher,
+                price = book.price,
+                createdAt = now,
+                updatedAt = now
+            )
 
-        template.update(entity).awaitFirst()
+            template.update(entity).awaitFirst()
+
+        } catch (e: DataAccessException) {
+            throw ResourceAccessError(e, "BookRepository#updateでエラーが発生しました")
+        }
     }
 }
